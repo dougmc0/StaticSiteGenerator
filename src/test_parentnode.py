d87f44c292403dd7a0067d57c13998b3b401a971
dougmc0 <dougmc@frenzied.us>
2025-02-24 20:05:33 -0600

    .

--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -19,5 +19,23 @@ class TestParentNode(unittest.TestCase):
         text2 = '<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>'
         self.assertEqual(text1, text2)
 
+    def test2(self):
+        node = ParentNode(
+            "p",
+            [
+                ParentNode("b",
+                    [
+                        LeafNode("b", "Bold text"),
+                        LeafNode(None, "Normal text"),
+                        LeafNode("p", "Normal text2"),
+                    ]
+                )
+            ],
+        )
+        text1 = node.to_html()
+        text2 = '<p><b><b>Bold text</b>Normal text<p>Normal text2</p></b></p>'
+        self.assertEqual(text1, text2)
+
+
 if __name__ == "__main__":
     unittest.main()
