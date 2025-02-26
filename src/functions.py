a5005d3fcbfec44b2a08d486fa4892be841ee996
dougmc0 <dougmc@frenzied.us>
2025-02-26 15:14:24 -0600

    .

--- src/functions.py
+++ src/functions.py
@@ -91,3 +91,7 @@ def text_to_textnodes(text):
     nodes = split_nodes_image(nodes)
     nodes = split_nodes_link(nodes)
     return nodes
+
+def markdown_to_blocks(markdown):
+    blocks = markdown.split("\n\n")
+    return blocks
--- src/test_functions.py
+++ src/test_functions.py
@@ -104,6 +104,26 @@ def test_text_to_textnodes(self):
         TextNode("link", TextType.LINK, "https://boot.dev"),
     ]
     self.assertListEqual(nodes, expected)
+
+
+def test_markdown_to_blocks(self):
+    md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+    blocks = markdown_to_blocks(md)
+    self.assertEqual(
+        blocks,
+        [
+            "This is **bolded** paragraph",
+            "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+            "- This is a list\n- with items",
+        ],)
                              
 
 
